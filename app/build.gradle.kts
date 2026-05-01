61b05608145a03795be9c688e5f0f91724cb1c47
dahyeon-bang <dahyeon.bang@linecorp.com>
2026-05-01 20:58:48 +0900

    release: Focus Flow v1.8.4 (Environment & Patch Stabilization)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.focusflow.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 50
-        versionName = "1.8.3"
+        versionCode = 51
+        versionName = "1.8.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
